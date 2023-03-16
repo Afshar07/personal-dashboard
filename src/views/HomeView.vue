6561459232953b6e0641ef95cb0f1e7e62c663f4
Afshar07 <mr.afshar0@gmail.com>
2023-03-17 02:00:05 +0330

    Configure tailwind css to use

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="w-full">
+    <div class="bg-red-800 w-full text-red-700">
+      <p class="text-white">hey</p>
+    </div>
   </main>
 </template>
